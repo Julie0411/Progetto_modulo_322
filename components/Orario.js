d3ca468691725a4bb10ce93bc9d0574b28fb2487
tarakanillius <tarakanillius@proton.me>
2024-10-29 16:18:56 +0100

    Revert "first commit"
    This reverts commit 04ffc3473fbfa5339aa9e64f8667f6bd569a5cd9.

--- components/Orario.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Calendar from "./Calendar";
-
-export default function Orario({darkThemeEnabled}){
-    return (
-        <Calendar darkThemeEnabled={darkThemeEnabled}/>
-    );
-}
