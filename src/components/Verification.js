ca0f938a113325bb89302af5e18e65c01625a179
tarakanillius <tarakanillius@proton.me>
2024-11-04 14:32:13 +0100

    minor update

--- src/components/Verification.js
+++ src/components/Verification.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Pressable, Dimensions } from 'react-native';
 import { useStyles } from "../utils/hooks/useStyles";
-
-const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const formattedDate = new Intl.DateTimeFormat('it-IT', { day: 'numeric', month: 'long' }).format(date);
-    return formattedDate.slice(0, 2) + formattedDate.charAt(2).toUpperCase() + formattedDate.slice(3);
-};
+import dateFormatter from "../utils/formatters/dateFormatter";
 
 const createStyles = (colors) => StyleSheet.create({
     container: {
@@ -22,16 +17,16 @@ const createStyles = (colors) => StyleSheet.create({
         height: 60,
     },
     pressable: {
-        flexDirection: 'row', // Set the Pressable to be a row as well
+        flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        flex: 1, // Allow it to grow and fill the container
+        flex: 1,
         padding: 10,
     },
     text: {
         color: colors.text,
-        flexShrink: 1, // Prevent the text from growing larger than its container
-        marginRight: 10, // Add some space between the text items
+        flexShrink: 1,
+        marginRight: 10,
     },
     pressedItem: {
         backgroundColor: colors.surface,
@@ -49,10 +44,12 @@ const Verification = ({ item, darkThemeEnabled, press, longPress }) => {
                 style={({ pressed }) => [styles.pressable, pressed && styles.pressedItem]}
             >
                 <Text style={styles.text} numberOfLines={1}>{item.title}</Text>
-                <Text style={styles.text} numberOfLines={1}>{formatDate(item.data.dateTime)}</Text>
+                <Text style={styles.text} numberOfLines={1}>
+                    {dateFormatter.formatDate(item.data.dateTime)}
+                </Text>
             </Pressable>
         </View>
     );
-}
+};
 
 export default Verification;
--- src/utils/formatters/dateFormatter.js
+++ src/utils/formatters/dateFormatter.js
@@ -1,14 +1,14 @@
 export const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const formattedDate = new Intl.DateTimeFormat('it-IT', {
-        day: 'numeric',
-        month: 'long'
-    }).format(date);
-    return formattedDate.slice(0, 2) +
-        formattedDate.charAt(2).toUpperCase() +
-        formattedDate.slice(3);
+    const formattedDate = new Intl.DateTimeFormat('it-IT', { day: 'numeric', month: 'long' }).format(date);
+    return formattedDate.slice(0, 2) + formattedDate.charAt(2).toUpperCase() + formattedDate.slice(3);
 };
 
 export const formatEventTime = (dateTime) => {
     return dateTime.slice(11, 16);
+};
+
+export default {
+    formatDate,
+    formatEventTime,
 };
