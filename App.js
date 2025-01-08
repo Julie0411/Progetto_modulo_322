278c4e0247b0904e43298efaec198ad520973d8f
Julie <julie.gambale@gmail.com>
2025-01-08 09:23:40 +0100

    minimal changes

--- App.js
+++ App.js
@@ -87,10 +87,6 @@ export default function App() {
             <StatusBar barStyle={darkThemeEnabled ? 'light-content' : 'dark-content'}/>
             <NavigationContainer theme={theme}>
                 <Stack.Navigator>
-                    <Stack.Screen
-                        name="TabNavigator"
-                        options={{headerShown: false}}
-                    >
                     <Stack.Screen name="TabNavigator" options={{headerShown: false}}>
                         {(navigation) => (
                             <TabNavigator
@@ -153,7 +149,6 @@ export default function App() {
                                 sortAscending={sortAscending}
                             />
                         )}
-                    </Stack.Screen>
                     </Stack.Screen>
                 </Stack.Navigator>
             </NavigationContainer>
