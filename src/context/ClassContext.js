f002338373de6fe7f5e025b8990e46727035c6ad
tarakanillius <tarakanillius@proton.me>
2025-04-14 15:38:03 +0200

    fix

--- src/context/ClassContext.js
+++ src/context/ClassContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const ClassContext = createContext();
 
 export const ClassSettingsProvider = ({ children }) => {
-    const [selectedClass, setSelectedClass] = useState({label: "I2a", maturityIsEnabled: true});
+    const [selectedClass, setSelectedClass] = useState();
     const [maturityIsEnabled, setMaturityIsEnabled] = useState(false);
 
     const toggleMaturity = () => setMaturityIsEnabled(!maturityIsEnabled);
